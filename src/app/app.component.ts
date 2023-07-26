25495d5c4a01c84f73070af5e89c1cc452d1bb10
JeoungMin.Seo <jeoungmin.seo@gaubiz.com>
2023-07-26 11:24:56 +0900

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gorder-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'gorder';
+}
